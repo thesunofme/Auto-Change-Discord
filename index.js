42e05ec5cd709f66a8708d1d448d7a0708bf138b
cronjob <thesunofme1@gmail.com>
2024-01-17 02:22:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  readyStatus: true,
+  checkUpdate: true
+});
 const keepAlive = require("./server")
 const request = require('request');
 const config = require('./config.json');
@@ -10,6 +15,43 @@ keepAlive()
 console.log('Discord Custom Status Changer by https://github.com/zbzyy');
 console.log('Modified by MrDarkShadoow');
 console.log('Your status will be updated in few seconds');
+
+const keepAlive = require('./server.js')
+keepAlive()
+
+client.on('ready', async () => {
+
+
+    console.clear();
+
+    console.log(`${client.user.tag} - rich presence started!`
+               )
+const r = new Discord.RichPresence()
+    .setApplicationId('1076934775404757002')
+    .setType('PLAYING')
+    .setURL('https://www.youtube.com/watch?v=4UAGmtDEPSc')
+    .setState('ㅤㅤㅤ🌸◕ ‿ ◕🌸ㅤhi!')
+  
+    .setName('💸better than yesterday💰')
+    .setDetails('"𝘐 𝘸𝘰𝘯ʹ𝘵 𝘣𝘪𝘵𝘦 𝘪𝘧 𝘺𝘰𝘶ʹ𝘳𝘦 𝘯𝘪𝘤𝘦 𝘵𝘰 𝘮𝘦"')
+    /*.setParty({
+        max: 9999,
+        current: 6789,
+        id: Discord.getUUID(),
+    })*/
+    
+    .setStartTimestamp(Date.now())
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1078967664363438181/1175103459175956480/giphy.gif')
+    .setAssetsLargeText('_reelDuckZ')
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1085934377680253031/1086385577865261056/giphy.gif')
+    .setAssetsSmallText('Verify User')
+    .addButton('Nghe nhạc ko ?', 'https://www.youtube.com/watch?v=ybaISsqERqg')
+    .addButton('Discord Server', 'https://discord.gg/9ad8a4YM4nz')
+     client.user.setActivity(r);
+     client.user.setPresence({ status: "dnd" });
+})
+
+client.login(process.env.TOKEN)
 
 setInterval(() => {
     request({
